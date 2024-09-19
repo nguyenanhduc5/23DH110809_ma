18267e4c577dcd85ee29d0e2c33cb396a19cb4de
SV <SV@PM19-PC02>
2024-09-19 15:07:44 +0700

    Add project files.

---
+++ 23DH110809_ma/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH110809_ma.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult trangchu()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 23DH110809_ma/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23DH110809_ma.MvcApplication" Language="C#" %>
